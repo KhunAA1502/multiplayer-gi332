5f31bbb3c23d38ec5e3a667bb4cd66b1b2585252
KhunAA1502 <ying010947@gmail.com>
2026-02-05 11:39:07 +0700

    1

---
+++ Assets/Scrip/Core/Player/ProjectileLauncher.cs
@@ -0,0 +1,76 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class ProjectileLauncher : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject serverProjectilePrefab;
+    [SerializeField] private GameObject clientProjectilePrefab;
+
+    [Header("Settings")]
+    [SerializeField] private float projectileSpeed;
+
+    private bool shouldFire;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) { return; }
+
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner) { return; }
+
+        inputReader.PrimaryFireEvent -= HandlePrimaryFire;
+    }
+
+    private void Update()
+    {
+        if (!IsOwner) { return; }
+
+        if (!shouldFire) { return; }
+
+        PrimaryFireServerRpc(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+    }
+
+    private void HandlePrimaryFire(bool shouldFire)
+    {
+        this.shouldFire = shouldFire;
+    }
+
+    [Rpc(SendTo.Server)]
+    private void PrimaryFireServerRpc(Vector3 spawnPos, Vector3 direction)
+    {
+        GameObject projectileInstance = Instantiate(
+            serverProjectilePrefab,
+            spawnPos,
+            Quaternion.identity);
+
+        projectileInstance.transform.up = direction;
+
+        SpawnDummyProjectileClientRpc(spawnPos, direction);
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void SpawnDummyProjectileClientRpc(Vector3 spawnPos, Vector3 direction)
+    {
+        if (IsOwner) { return; }
+
+        SpawnDummyProjectile(spawnPos, direction);
+    }
+
+    private void SpawnDummyProjectile(Vector3 spawnPos, Vector3 direction)
+    {
+        GameObject projectileInstance = Instantiate(
+            clientProjectilePrefab,
+            spawnPos,
+            Quaternion.identity);
+
+        projectileInstance.transform.up = direction;
+    }
+}
