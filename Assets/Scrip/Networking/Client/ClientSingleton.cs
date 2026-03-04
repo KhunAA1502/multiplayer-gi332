287fd4f8de18ea86bf271ad85783fa6414e9aa84
KhunAA1502 <ying010947@gmail.com>
2026-03-05 00:11:17 +0700

    Week7

--- Assets/Scrip/Networking/Client/ClientSingleton.cs
+++ Assets/Scrip/Networking/Client/ClientSingleton.cs
@@ -9,27 +9,32 @@ public class ClientSingleton : MonoBehaviour
     {
         get
         {
-            if(instance != null) { return instance; }
+            if (instance != null) { return instance; }
             instance = FindFirstObjectByType<ClientSingleton>();
 
-            if(instance == null)
+            if (instance == null)
             {
                 Debug.LogError("No ClientSingleton in the scene!");
                 return null;
             }
-            return instance;    
+            return instance;
         }
     }
     void Start()
     {
-        DontDestroyOnLoad(gameObject);  
+        DontDestroyOnLoad(gameObject);
     }
 
     public async Task<bool> CreateClient()
     {
-        GameManager = new ClientGameManager();  
+        GameManager = new ClientGameManager();
 
         return await GameManager.InitAsync();
+    }
+
+    private void OnDestroy()
+    {
+        GameManager?.Dispose();
     }
 }
  
